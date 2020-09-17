a7b35762a1c32a25f69778db95b503f46bd09928
Nathan Keller <keller.nathan@gmail.com>
2020-09-17 11:30:38 -0600

    Create deck spec, initize with cards in an array determine rank of card

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,8 +3,65 @@ require_relative '../lib/card'
 
 RSpec.describe 'Deck' do
   it "exits" do
-    deck = Deck.new
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
 
     expect(deck).to be
+  end
+
+  it "contains an array of cards, empty" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = []
+
+    deck = Deck.new(cards)
+
+
+    expect(deck.cards).to eq([])
+  end
+
+  it 'contains cards in an array' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck =  Deck.new(cards)
+
+    expect(deck.cards).to eq([card1, card2, card3])
+  end
+
+  it "can verify the rank of a card at index position" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck =  Deck.new(cards)
+
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+  end
+
+  it "provides an array of cards that are high ranking, >= 11" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck =  Deck.new(cards)
+
+    
+
   end
 end
